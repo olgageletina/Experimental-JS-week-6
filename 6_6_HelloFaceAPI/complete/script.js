f283e1f073a206f6ae9fa8b463152e1421616dbe
olgageletina <olgaguillotine@gmail.com>
2020-09-29 21:07:05 -0400

    first module

---
+++ 6_6_HelloFaceAPI/complete/script.js
@@ -0,0 +1,35 @@
+const cElem = document.getElementById('canvas');
+const video = document.getElementById('video');
+
+//load asynchronously
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('../../models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('../../models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('../../models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('../../models')
+]).then(startVideo)
+
+//hook up webcam to video element
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {} },
+        //coming from the webcam
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+//event listener
+video.addEventListener('play', () => {
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(cElem, displaySize);
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const resizeDetections = faceapi.resizeResults(detections, displaySize);
+        cElem.getContext('2d').clearRect(0, 0, cElem.width, cElem.height);
+        faceapi.draw.drawDetections(canvas, resizeDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizeDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizeDetections);
+    }, 100)
+})
